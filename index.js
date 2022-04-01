c2d2c562badd1ef3edc1c10d49234803cd74ab79
brian <brian.w.727@gmail.com>
2022-04-01 11:09:05 -0700

    locations now stored

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const { response } = require('express');
-//NeDB
-const Datastore = require('nedb');
-//express
-const { request } = require('express');
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-//giving path to a filename
+
 const database = new Datastore('database.db');
-//loads data into memory
 database.loadDatabase();
 
 
 app.post('/api', (request, response) => {
-    console.log(request.body);
+    console.log('I got a request');
     const data = request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
@@ -25,7 +20,6 @@ app.post('/api', (request, response) => {
         status: 'success',
         latitude: data.lat,
         longitude: data.lon,
-        //adds timestamp to JSON object
         timestamp: timestamp
     });
 });
